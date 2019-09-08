611c315d58290aadef157d4687e686568e378692
arturoalviar <arturoalviar@gmail.com>
2019-09-08 15:39:27 -0400

    include app version in dmg name

--- .electron-vue/buildDmg.js
+++ .electron-vue/buildDmg.js
@@ -1,9 +1,9 @@
 const path = require('path')
 const buildDmg = require('electron-installer-dmg')
-
+const VERSION = process.env.npm_package_version;
 const options = {
   appPath: path.join(__dirname, '../build/vhue-darwin-x64/vhue.app'),
-  name: 'vhue',
+  name: `vhue-${VERSION}`,
   title: 'Install vhue',
   icon: path.join(__dirname, '../build/icons/icon.icns'),
   out: path.join(__dirname, '../build/dmg'),
